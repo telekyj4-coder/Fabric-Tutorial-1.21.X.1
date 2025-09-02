3a682f1bbeb2a83d7d8d6f7f3758aba771205d6c
Jonas Teleky <teleky.j4@gmail.com>
2025-09-02 21:09:24 +0200

    Initial commit mit TutorialMod.java

--- src/main/java/net/joszen/tutorialmod/TutorialMod.java
+++ src/main/java/net/joszen/tutorialmod/TutorialMod.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 //Very important Comment
+//Testing
 public class TutorialMod implements ModInitializer {
     public static final String MOD_ID = "tutorialmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
